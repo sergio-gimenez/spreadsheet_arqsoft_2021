5770e81540751f402c6fbe9500649b64fc984244
Sergio Gimenez <sergiogimenezanton@gmail.com>
2021-05-18 15:54:24 -0400

    [WIP] Building project skeleton

---
+++ spreadsheet/src/main/java/edu/upc/etsetb/arqsoft/spreadsheet/entities/Cell.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.upc.etsetb.arqsoft.spreadsheet.entities;
+
+/**
+ *
+ * @author osboxes
+ */
+public class Cell {
+    private Content content;
+    
+}
